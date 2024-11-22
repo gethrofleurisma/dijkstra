e0a929e415bf5b4b2803564bb431f3daf58b8bce
Khalil Bohner <khalil.bohner@ixcsoft.com.br>
2024-11-22 20:01:57 -0300

    Adiciona mais informacoes sobre o dijkstra

--- index.py
+++ index.py
@@ -1,22 +1,22 @@
 def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
+    mensagem = "Edsger Wybe Dijkstra (1930–2002) foi um renomado cientista da computação holandês, amplamente reconhecido por suas contribuições fundamentais para a ciência da computação teórica e prática."
     return mensagem
 
+def resumo():
+    mensagem = "Edsger Wybe Dijkstra (1930–2002) foi um renomado cientista da computação holandês, amplamente reconhecido por suas contribuições fundamentais para a ciência da computação teórica e prática."
+    return mensagem
 
 def doutorado():
-    mensagem = ""
+    mensagem = "Dijkstra obteve seu doutorado na Universidade de Leiden em 1959, com uma tese sobre a solução numérica de equações diferenciais, antes de se dedicar à computação, que na época era um campo emergente."
     return mensagem
-
 
 def contribuicoes():
     mensagem = "Dijkstra produziu um vasto número de escritos, muitos em forma de notas manuscritas conhecidas como EWDs. Ele frequentemente defendia abordagens matemáticas rigorosas e criticava práticas que não seguiam princípios sólidos de engenharia."
     return mensagem
 
-
 def artigos():
-    mensagem = ""
+    mensagem = "Seus artigos e textos tiveram um impacto profundo em áreas como algoritmos, linguagens de programação e fundamentos teóricos da computação. "
     return mensagem
-
 
 def citacoes():
     mensagem = "A simplicidade é pré-requisito para a confiabilidade; A ciência da computação não trata de computadores, assim como a astronomia não trata de telescópios"
